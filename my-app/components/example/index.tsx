77484fc450335d418a0c25c1bc84a84415ecbfa0
kaoru.ohtaka <ockacru+git@gmail.com>
2021-07-04 12:20:58 +0900

    Setup tailwindcss

---
+++ my-app/components/example/index.tsx
@@ -0,0 +1,7 @@
+export function Example(): JSX.Element {
+  return (
+    <div className='text-9xl font-bold w-full flex flex-col justify-center items-center py-10 after:block after:w-full after:h-1 after:bg-red-100'>
+      Example
+    </div>
+  )
+}
