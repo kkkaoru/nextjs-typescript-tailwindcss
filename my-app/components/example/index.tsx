3e94039513cbfa4efe70ec0cf4cd3194e093db6c
kaoru.ohtaka <ockacru+git@gmail.com>
2021-07-05 07:14:02 +0900

    Clean by eslint

--- my-app/components/example/index.tsx
+++ my-app/components/example/index.tsx
@@ -1,6 +1,6 @@
 export function Example(): JSX.Element {
   return (
-    <div className="text-9xl font-bold w-full flex flex-col justify-center items-center py-10 after:block after:w-full after:h-1 after:bg-red-100">
+    <div className="flex flex-col items-center justify-center w-full py-10 font-bold text-9xl after:block after:w-full after:h-1 after:bg-red-100">
       Example
     </div>
   );
