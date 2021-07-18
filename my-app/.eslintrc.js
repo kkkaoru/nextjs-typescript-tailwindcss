e751ed22c8f133139409613ef6afde9b9b5ad6f3
Kaoru <46368397+kkkaoru@users.noreply.github.com>
2021-07-18 12:33:59 +0900

    Setup nextjs (#1)
    * Add files by next-app
    
    * Setup tailwindcss
    
    * Add prettier
    
    * Clean by prettier
    
    * Add eslint
    
    * Clean by eslint
    
    * Ignores eslint
    
    * Add lint-staged
    
    * Add lint by ci

---
+++ my-app/.eslintrc.js
@@ -0,0 +1,22 @@
+/** @type import('eslint').Linter.BaseConfig */
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-typescript',
+    'plugin:tailwind/recommended',
+    'next',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'max-lines': 'error',
+    'max-depth': 'error',
+    'max-lines-per-function': 'error',
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
