38f52bdccf1fe3bc9bcd7e50bd7c3686d1d9dcaa
kaoru.ohtaka <ockacru+git@gmail.com>
2021-08-15 07:54:05 +0900

    Add new addons for storybook

--- packages/web-components/.storybook/main.js
+++ packages/web-components/.storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
+    '@storybook/addon-viewport',
     '@storybook/addon-links',
     '@storybook/addon-actions',
     '@storybook/addon-essentials',
