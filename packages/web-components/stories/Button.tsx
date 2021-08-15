db04400ed989bf502081ec43af6801ceaea331da
kaoru.ohtaka <ockacru+git@gmail.com>
2021-08-15 09:35:25 +0900

    Remove css file

--- packages/web-components/stories/Button.tsx
+++ packages/web-components/stories/Button.tsx
@@ -1,25 +1,36 @@
 import type { MouseEventHandler } from 'react';
-import './button.css';
 
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+
+type ButtonSize = 'small' | 'medium' | 'large';
 
 type Props = {
   primary: boolean;
   backgroundColor: string | null;
-  size: 'small' | 'medium' | 'large';
+  size: ButtonSize;
   label: string;
   onClick: MouseEventHandler<HTMLButtonElement> | undefined;
 } & ButtonProps;
+
+const buttonSizeClassNames = new Map<ButtonSize, string>([
+  ['small', 'text-xs py-[10px] px-[16px]'],
+  ['medium', 'text-base py-[11px] px-[20px]'],
+  ['large', 'text-xl py-[12px] px-[24px]'],
+])
 
 /**
  * Primary UI component for user interaction
  */
 export const Button: React.VFC<Props> = ({ backgroundColor = null, primary = false, size = 'medium', onClick = undefined, label, ...buttonProps }): JSX.Element => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const buttonSizeClass = buttonSizeClassNames.get(size);
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={[
+        'font-sans font-bold border-0 rounded-[3em] cursor-pointer inline-block leading-none',
+        `${buttonSizeClass}`,
+        primary ? 'text-[#fff] bg-[#1ea7fd]' : 'text-[#333] bg-transparent shadow-md'
+      ].join(' ')}
       style={backgroundColor && { backgroundColor }}
       onClick={ onClick !== undefined && onClick}
       {...buttonProps}
