46d12d265433fc7d32c738c73890431b300d0f3f
kaoru.ohtaka <ockacru+git@gmail.com>
2021-08-22 04:11:15 +0900

    Setup jest in web-components

---
+++ packages/web-components/jest.config.js
@@ -0,0 +1,33 @@
+/** @type import('@jest/types/build/Config').InitialOptions */
+module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      '@swc/jest',
+      {
+        /** @type import('@swc/core/types').JscConfig */
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '~/(.*)$': '<rootDir>/$1',
+    '@/(.*)$': '<rootDir>/$1',
+  },
+};
