bfc048f1554975baf7c91ea7ea02f04305e05b6f
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-05 04:57:15 +0900

    Add example about can import local package in next.js

--- packages/web-client/pages/index.tsx
+++ packages/web-client/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { Example } from '../components/example';
+import { Example } from 'web-components/src/components/atoms/example/Example';
+import { Button } from 'web-components/src/components/atoms/button/Button';
 
 // eslint-disable-next-line max-lines-per-function
 export default function Home(): JSX.Element {
@@ -13,6 +14,7 @@ export default function Home(): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Example />
+      <Button label="Example button" />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
