b87b95df0d7462974e4a71b2b65d15bf5b683bde
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-05 23:31:45 +0900

    Ignore type check by web-client in web-components file

--- packages/web-components/src/components/molecules/text-and-button/index.tsx
+++ packages/web-components/src/components/molecules/text-and-button/index.tsx
@@ -1,4 +1,8 @@
-import { DefaultButton, Props as ButtonProps } from '@web-components/components/atoms/button/default';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// Cannot safe by tsc when using in web-client
+// @ts-ignore
+import { DefaultButton, Props as ButtonProps } from '@web-components/components/atoms/button';
+// @ts-ignore
 import { TextWithUnderline } from '@web-components/components/atoms/text';
 
 export type Props = {
